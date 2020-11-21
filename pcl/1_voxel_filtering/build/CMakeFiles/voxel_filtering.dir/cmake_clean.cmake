04842c06c207b42aa88ed68022f4fd2b6df01f1f
Matteo Baiguera <baiguera@3dtarget.it>
2020-11-21 09:18:41 +0100

    added point-cloud exercitation source code

---
+++ pcl/1_voxel_filtering/build/CMakeFiles/voxel_filtering.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/voxel_filtering.dir/voxel_filtering.cpp.o"
+  "voxel_filtering"
+  "voxel_filtering.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/voxel_filtering.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
